4a1c8e34704a98204134cef41934796cce3591e8
iMiike1 <43544291+iMiike1@users.noreply.github.com>
2019-09-23 00:00:59 +0200

    Added Billboard Script + Imported player controller
    WASD - basic player controller
    QE - Camera rotation +/- 90° around empty pivot
    
    Drag Billboard script for every billboard obj

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public GameObject Camera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        if (moveHorizontal != 0)
+        {
+
+
+            Vector3 move = new Vector3(moveHorizontal, 0.0f, 0.0f);
+            transform.Translate(move * speed * Time.deltaTime, Space.Self);
+
+        }
+        if (moveVertical != 0)
+        {
+
+
+            Vector3 move2 = new Vector3(0.0f, 0.0f, moveVertical);
+            transform.Translate(move2 * speed * Time.deltaTime, Space.Self);
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+           float R = Camera.transform.eulerAngles.y;
+            Camera.transform.SetPositionAndRotation(Camera.transform.position, Quaternion.Euler(Camera.transform.eulerAngles.x, R - 90.0f, Camera.transform.eulerAngles.z));
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            float R = Camera.transform.eulerAngles.y;
+            Camera.transform.SetPositionAndRotation(Camera.transform.position, Quaternion.Euler(Camera.transform.eulerAngles.x, R + 90.0f, Camera.transform.eulerAngles.z));
+        }
+
+    }
+}
