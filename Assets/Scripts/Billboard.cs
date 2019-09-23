6ef91fa718e18b6dbbdb039563b4daac1d4d3c2d
iMiike1 <43544291+iMiike1@users.noreply.github.com>
2019-09-23 09:38:51 +0200

    Added, Mountain, Lake and empty GameManager

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -6,7 +6,7 @@ public class Billboard : MonoBehaviour
 {
     // Update is called once per frame
     void Update()
-    {
+    {   //X/Y ROTATION NEEDS TO BE BLOCKED
         transform.LookAt(Camera.main.transform.position,Vector3.up);
     }
 }
