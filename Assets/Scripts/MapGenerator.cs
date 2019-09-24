87e519218dd5e6e666af4d01db8bb403ee857a26
Alessio <alessio.calore96@gmail.com>
2019-09-24 14:37:39 +0200

    Added chunk generation
    when the player moves, planes will now appear around him, in varying levels of detail.
    Terrain mesh has yet to be implemented.

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public const int mapChunkSize = 241;
     public enum DrawMode {NoiseMap,colourMap, Mesh};
     public DrawMode drawMode;
-    public int mapWidth;
-    public int mapHeight;
     public float noiseScale;
 
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
     public int octaves;
     [Range(0,1)]
     public float persistance;
@@ -18,22 +19,24 @@ public class MapGenerator : MonoBehaviour
     public Vector2 offset;
     public bool autoUpdate;
 
+   [Range(0,6)]
+   public int levelOfDetail;
     public TerrainType[] regions;
     public void GenerateMap()
     {    
-        float [,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale,octaves,persistance,lacunarity,seed,offset);
+        float [,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, noiseScale,octaves,persistance,lacunarity,seed,offset);
 
-        Color[] colourMap = new Color[mapWidth * mapHeight];
-         for (int y = 0; y < mapHeight; y++)
+        Color[] colourMap = new Color[mapChunkSize * mapChunkSize];
+         for (int y = 0; y < mapChunkSize; y++)
         {
-            for (int x = 0; x < mapWidth; x++)
+            for (int x = 0; x < mapChunkSize; x++)
             {
                 float currentHeight = noiseMap[x,y];
                 for (int i = 0; i < regions.Length; i++)
                 {
                     if(currentHeight<=regions[i].height)
                     {
-                        colourMap[y*mapWidth+x] = regions[i].colour;
+                        colourMap[y*mapChunkSize+x] = regions[i].colour;
                         break;
                     }
                 }
@@ -45,22 +48,14 @@ public class MapGenerator : MonoBehaviour
         display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
         }else if(drawMode == DrawMode.colourMap)
         {
-            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth,mapHeight));
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize,mapChunkSize));
         }else if(drawMode == DrawMode.Mesh)
         {
-            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth,mapHeight));
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier,meshHeightCurve,levelOfDetail), TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize,mapChunkSize));
         }
     }
     void OnValidate()
     {
-        if(mapWidth<1)
-        {
-            mapWidth = 1;
-        }
-        if (mapHeight<1)
-        {
-            mapHeight = 1;
-        }
         if (lacunarity<1)
         {
             lacunarity = 1;
