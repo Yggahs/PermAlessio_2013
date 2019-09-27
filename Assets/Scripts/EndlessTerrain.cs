eac5e9e97e261b8d4df732ace5a5e400faa2ebf3
Alessio <alessio.calore96@gmail.com>
2019-09-27 17:03:09 +0200

    Added Collision,Flat Shading, Debug Controller, Seams aren't fixed
    seams will still appear between chunks of different LOD, causing players to fall off the map

--- Assets/Scripts/EndlessTerrain.cs
+++ Assets/Scripts/EndlessTerrain.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class EndlessTerrain : MonoBehaviour {
 
-	const float scale = 5f;
+	const float scale = 2f;
 	const float viewerMoveThresholdForChunkUpdate = 25f;
 	const float sqrViewerMoveThresholdForChunkUpdate = viewerMoveThresholdForChunkUpdate * viewerMoveThresholdForChunkUpdate;
 
@@ -75,9 +75,12 @@ public class EndlessTerrain : MonoBehaviour {
 
 		MeshRenderer meshRenderer;
 		MeshFilter meshFilter;
+		MeshCollider meshCollider;
 
 		LODInfo[] detailLevels;
 		LODMesh[] lodMeshes;
+
+		LODMesh collisionLODMesh;
 
 		MapData mapData;
 		bool mapDataReceived;
@@ -93,6 +96,7 @@ public class EndlessTerrain : MonoBehaviour {
 			meshObject = new GameObject("Terrain Chunk");
 			meshRenderer = meshObject.AddComponent<MeshRenderer>();
 			meshFilter = meshObject.AddComponent<MeshFilter>();
+			meshCollider = meshObject.AddComponent<MeshCollider>();
 			meshRenderer.material = material;
 
 			meshObject.transform.position = positionV3 * scale;
@@ -103,6 +107,10 @@ public class EndlessTerrain : MonoBehaviour {
 			lodMeshes = new LODMesh[detailLevels.Length];
 			for (int i = 0; i < detailLevels.Length; i++) {
 				lodMeshes[i] = new LODMesh(detailLevels[i].lod, UpdateTerrainChunk);
+				if(detailLevels[i].useForCollider)
+				{
+					collisionLODMesh = lodMeshes[i];
+				}
 			}
 
 			mapGenerator.RequestMapData(position,OnMapDataReceived);
@@ -145,6 +153,18 @@ public class EndlessTerrain : MonoBehaviour {
 							lodMesh.RequestMesh (mapData);
 						}
 					}
+
+					if(lodIndex == 0)
+					{
+						if(collisionLODMesh.hasMesh)
+						{
+							meshCollider.sharedMesh = collisionLODMesh.mesh;
+						}else if(!collisionLODMesh.hasRequestedMesh)
+						{
+							collisionLODMesh.RequestMesh(mapData);
+						}
+					}
+
 					terrainChunksVisibleLastUpdate.Add(this);
 				}
 
@@ -193,6 +213,7 @@ public class EndlessTerrain : MonoBehaviour {
 	public struct LODInfo {
 		public int lod;
 		public float visibleDstThreshold;
+		public bool useForCollider;	
 	}
 
 }
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -10,7 +10,9 @@ public class MapGenerator : MonoBehaviour {
 	public DrawMode drawMode;
 
 	public Noise.NormalizeMode normalizeMode;
-	public const int mapChunkSize = 239;
+
+	public bool useFlatShading;
+
 	[Range(0,6)]
 	public int editorPreviewLOD;
 	public float noiseScale;
@@ -30,13 +32,29 @@ public class MapGenerator : MonoBehaviour {
 	public bool autoUpdate;
 
 	public TerrainType[] regions;
-
+	static MapGenerator instance;
 	float[,] falloffMap;
 	Queue<MapThreadInfo<MapData>> mapDataThreadInfoQueue = new Queue<MapThreadInfo<MapData>>();
 	Queue<MapThreadInfo<MeshData>> meshDataThreadInfoQueue = new Queue<MapThreadInfo<MeshData>>();
 
 	void Awake() {
 		falloffMap = FalloffGenerator.GenerateFalloffMap(mapChunkSize);
+	}
+
+	public static int mapChunkSize
+	{
+		get
+		{
+			if(instance == null){
+				instance = FindObjectOfType<MapGenerator>();
+			}
+			if(instance.useFlatShading){
+				return 95;
+			}else
+			{
+				return 239;
+			}
+		}
 	}
 	public void DrawMapInEditor() {
 		MapData mapData = GenerateMapData (Vector2.zero);
@@ -47,7 +65,7 @@ public class MapGenerator : MonoBehaviour {
 		} else if (drawMode == DrawMode.ColourMap) {
 			display.DrawTexture (TextureGenerator.TextureFromColourMap (mapData.colourMap, mapChunkSize, mapChunkSize));
 		} else if (drawMode == DrawMode.Mesh) {
-			display.DrawMesh (MeshGenerator.GenerateTerrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, editorPreviewLOD), TextureGenerator.TextureFromColourMap (mapData.colourMap, mapChunkSize, mapChunkSize));
+			display.DrawMesh (MeshGenerator.GenerateTerrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, editorPreviewLOD, useFlatShading), TextureGenerator.TextureFromColourMap (mapData.colourMap, mapChunkSize, mapChunkSize));
 		}
 		else if (drawMode == DrawMode.FalloffMap)
 		{
@@ -79,7 +97,7 @@ public class MapGenerator : MonoBehaviour {
 	}
 
 	void MeshDataThread(MapData mapData, int lod, Action<MeshData> callback) {
-		MeshData meshData = MeshGenerator.GenerateTerrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, lod);
+		MeshData meshData = MeshGenerator.GenerateTerrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, lod,useFlatShading);
 		lock (meshDataThreadInfoQueue) {
 			meshDataThreadInfoQueue.Enqueue (new MapThreadInfo<MeshData> (callback, meshData));
 		}
